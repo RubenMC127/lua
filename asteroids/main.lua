87b13243523cb2f69f33ea14716fa9f11ce25535
rubenmc127 <rubenmcampos@gmail.com>
2022-12-18 23:11:39 +0100

    acceleration added

--- asteroids/main.lua
+++ asteroids/main.lua
@@ -1,40 +1,73 @@
+local function vector_magnitude(v)
+    return math.sqrt(math.pow(v.x, 2) + math.pow(v.y, 2))
+end
+
 function love.load()
     player = {} 
     player.x = 400
     player.y = 200
-    player.speed = 250
+    player.speed = 0--250
+    player.acceleration = 0
+    player.deceleration = player.acceleration
     player.sprite = love.graphics.newImage('sprites/ship.png')
-    player.rotation = 0
+    player.ship_facing_theta = 0
+    player.ship_forward_vector = {x=1,y=0}
     player.rotation_speed = 5
     background = {
         love.graphics.newImage('sprites/background_0.png'),
         love.graphics.newImage('sprites/background_1.png')
     }
+    window = {
+        width = 800,
+        height = 600
+    }
 end
 
 function love.update(dt)
     if love.keyboard.isDown("right") then
-        player.rotation = player.rotation + player.rotation_speed*dt 
+        player.ship_facing_theta = player.ship_facing_theta + player.rotation_speed*dt 
     end
 
     if love.keyboard.isDown("left") then
-        player.rotation = player.rotation - player.rotation_speed*dt 
+        player.ship_facing_theta = player.ship_facing_theta - player.rotation_speed*dt 
     end
 
     if love.keyboard.isDown("up") then
-        player.x = player.x + math.cos(player.rotation) * player.speed * dt
-        player.y = player.y + math.sin(player.rotation) * player.speed * dt
+        player.acceleration = 200
+    elseif love.keyboard.isDown("down") then
+        player.acceleration = -200
+    else
+        player.acceleration = 0
     end
     
-    if love.keyboard.isDown("down") then
-        player.x = player.x - math.cos(player.rotation) * player.speed * dt
-        player.y = player.y - math.sin(player.rotation) * player.speed * dt
+    -- Adding maximum speed
+    if player.speed <= 300 then
+        player.speed = player.speed + player.acceleration*dt
+    else
+        player.speed = 300
     end
+
+    -- Always update ship position
+    local new_x = player.x + math.cos(player.ship_facing_theta) * player.speed * dt
+    if new_x > window.width then
+        new_x = new_x - window.width
+    elseif new_x < 0 then
+        new_x = window.width + new_x
+    end
+    player.x = new_x
+
+    local new_y = player.y + math.sin(player.ship_facing_theta) * player.speed * dt
+    if new_y > window.height then
+        new_y = new_y - window.height
+    elseif new_y < 0 then
+        new_y = window.height + new_y
+    end
+    player.y = new_y
 end
 
 function love.draw()
     love.graphics.draw(background[1], 0, 0)
-    love.graphics.draw(player.sprite, player.x, player.y, player.rotation, 0.5, 0.5, 60, 78)
+    love.graphics.draw(player.sprite, player.x, player.y, player.ship_facing_theta, 0.5, 0.5, 60, 78)
 end
 
 function love.keypressed(key, u)
